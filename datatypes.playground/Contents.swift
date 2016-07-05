398ca4a87590f213d8fd416a713d17b08a76c2ae
pressstart101 <walterscott404@yandex.com>
2016-07-05 14:16:18 -0400

    d

---
+++ datatypes.playground/Contents.swift
@@ -0,0 +1,12 @@
+let thirdPlanet = "Earth"
+var ninthPlanet:String
+ninthPlanet = "Pluto"
+let fifthPlanet: String = "Jupiter" //string
+let sixthPlanet: String = "Saturn" //string
+let seventhPlanet: String = "Uranus" //string
+let numberOfCountriesInAfrica = 54
+//let sixthPlanet = "Saturn"
+let numberOfMoonsOfSaturn = 62
+
+
+print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet) ")
